036a1ad11b39cb8047e007c58e5b409fbfa3d746
NewTX <32112628+NewTX@users.noreply.github.com>
2023-01-12 09:34:54 +0800

    Update diy-part1.sh

--- router-config/RASPBERRY-PI-3B-3BPLUS-CM-LIONELV21/diy-part1.sh
+++ router-config/RASPBERRY-PI-3B-3BPLUS-CM-LIONELV21/diy-part1.sh
@@ -14,4 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/NewTX/Lienol-openwrt' feeds.conf.default
+sed -i '$a src-git mylienol https://github.com/NewTX/Lienol-openwrt' feeds.conf.default
