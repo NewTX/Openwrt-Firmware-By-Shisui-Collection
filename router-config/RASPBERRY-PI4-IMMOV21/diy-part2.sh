3bc1724d3b419129c8b8f2d2b4d3a28e3a05cde5
NewTX <32112628+NewTX@users.noreply.github.com>
2023-01-12 15:26:14 +0800

    Update diy-part2.sh

--- router-config/RASPBERRY-PI4-IMMOV21/diy-part2.sh
+++ router-config/RASPBERRY-PI4-IMMOV21/diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # 移除 SNAPSHOT 标签
 sed -i 's,-SNAPSHOT,,g' include/version.mk
